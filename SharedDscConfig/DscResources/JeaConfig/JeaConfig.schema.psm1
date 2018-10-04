46bd329c28b834917b888d8c1da86b2b82fa139b
Chris Gardner <chris.gardner@section8games.co.uk>
2018-10-04 11:11:58 +0100

    add JeaConfig shared configuration

---
+++ SharedDscConfig/DscResources/JeaConfig/JeaConfig.schema.psm1
@@ -0,0 +1,21 @@
+Configuration JeaConfig {
+    Param(
+        $EndPoints = @(),
+        $RoleCapabilities = @()
+    )
+
+    Import-DscResource -ModuleName Jea
+
+    foreach ($Role in $RoleCapabilities) {
+        if(!$Role.Ensure) { $Role.add('Ensure', 'Present') }
+        $Name = $Role.Name
+        $Role.Remove('Name')
+        (Get-DscSplattedResource -ResourceName 'JeaRoleCapabilities' -ExecutionName "$($Name)_rc" -Properties $Role -NoInvoke).Invoke($Role)
+    }
+
+    foreach ($Endpoint in $EndPoints) {
+        if(!$Endpoint.Ensure) { $Endpoint.add('Ensure', 'Present') }
+        (Get-DscSplattedResource -ResourceName 'JeaEndpoint' -ExecutionName "$($Endpoint.EndpointName)_ep" -Properties $Endpoint -NoInvoke).Invoke($Endpoint)
+    }
+
+}
